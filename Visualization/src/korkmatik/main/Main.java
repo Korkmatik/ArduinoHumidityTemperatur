2223caedd70c3d9a5180b222b1e5718344390fa9
Korkmatik <9c54efc1-143b-4f5f-9d2f-f6f189583f0f@aleeas.com>
2020-04-17 22:15:35 +0200

    Added Main

---
+++ Visualization/src/korkmatik/main/Main.java
@@ -0,0 +1,12 @@
+package korkmatik.main;
+
+import korkmatik.main.view.SettingsView;
+
+public class Main {
+
+	public static void main(String[] args) {
+		SettingsView settings = new SettingsView();
+		settings.start();
+	}
+
+}

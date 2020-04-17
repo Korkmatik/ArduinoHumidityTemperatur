c7e55e53851d76a5bd8a0298337d55985e81e45e
Korkmatik <9c54efc1-143b-4f5f-9d2f-f6f189583f0f@aleeas.com>
2020-04-17 18:41:22 +0200

    Created Sender class

---
+++ Sensor/Sender.cpp
@@ -0,0 +1,30 @@
+/*
+ * Sender.cpp
+ *
+ *  Created on: 17.04.2020
+ *      Author: fahri
+ */
+
+#include "Sender.h"
+
+#include <HardwareSerial.h>
+
+Sender::Sender() { }
+
+Sender::~Sender() { }
+
+void Sender::sendTemperature(float temperature) {
+
+	// Communication protocol
+	Serial.print("(TEMP#");
+	Serial.print(temperature, 1);
+	Serial.print(")");
+}
+
+void Sender::sendHumidity(float humidity) {
+
+	// Communication protocol for humidity
+	Serial.print("(HUMI#");
+	Serial.print(humidity, 1);
+	Serial.print(")");
+}
---
+++ Sensor/Sender.h
@@ -0,0 +1,20 @@
+/*
+ * Sender.h
+ *
+ *  Created on: 17.04.2020
+ *      Author: fahri
+ */
+
+#ifndef SENDER_H_
+#define SENDER_H_
+
+class Sender {
+public:
+	Sender();
+	virtual ~Sender();
+
+	void sendTemperature(float temperature);
+	void sendHumidity(float humidity);
+};
+
+#endif /* SENDER_H_ */

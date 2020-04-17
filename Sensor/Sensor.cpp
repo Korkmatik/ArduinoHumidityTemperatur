a57c2d007cd3d9d54814291fa85a20d3bb07f418
Korkmatik <9c54efc1-143b-4f5f-9d2f-f6f189583f0f@aleeas.com>
2020-04-17 18:55:46 +0200

    Clean up work

--- Sensor/Sensor.cpp
+++ Sensor/Sensor.cpp
@@ -1,9 +1,41 @@
-#include <Arduino.h>
+#include <HardwareSerial.h>
+
+#include "dht_nonblocking.h"
+#include "Sender.h"
+#include "Helper.h"
+
+#define DHT_SENSOR_TYPE DHT_TYPE_11
+
+static const int DHT_SENSOR_PIN = 2;
+DHT_nonblocking dhtSensor( DHT_SENSOR_PIN, DHT_SENSOR_TYPE );
+
+Sender* sender;
+unsigned long max;
 
 void setup() {
+	Serial.begin(9600);
 
+	sender = new Sender();
+	max = 3000ul;
 }
+
 
 void loop() {
+	float temperature;
+	float humidity;
 
+	// Check if new message should be send
+	bool isMeasurementAvailable = Helper::measureEnvironment(
+			&temperature,
+			&humidity,
+			max,
+			&dhtSensor
+	);
+
+	if (isMeasurementAvailable == true) {
+		// sending a new message
+		sender->sendTemperature(temperature);
+		sender->sendHumidity(humidity);
+	}
 }
+

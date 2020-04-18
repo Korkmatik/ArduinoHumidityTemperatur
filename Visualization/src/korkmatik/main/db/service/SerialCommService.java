718251aabbb4dd2d01af3109554b3d1a6159b13b
Korkmatik <9c54efc1-143b-4f5f-9d2f-f6f189583f0f@aleeas.com>
2020-04-18 23:33:16 +0200

    Added port name

--- Visualization/src/korkmatik/main/db/service/SerialCommService.java
+++ Visualization/src/korkmatik/main/db/service/SerialCommService.java
@@ -4,6 +4,11 @@ import com.fazecast.jSerialComm.SerialPort;
 
 public class SerialCommService {
 
+	private String portName;
+	
+	public SerialCommService(String serialPortName) {
+		portName = serialPortName;
+	}
 	
 	public static SerialPort[] getSerialPorts() {
 		return SerialPort.getCommPorts();
